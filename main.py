2047f96256dd5c29aaf59fd36276bfb84276658d
Руслан Прусаков <ruslan.prusakov@gmail.com>
2022-07-01 13:43:43 +0300

    end

--- main.py
+++ main.py
@@ -6,21 +6,27 @@ file_name = '4.txt'
 
 list_of_files = os.listdir('files')
 list_dict = {}
-for file in list_of_files:
-    # print(file)
-    with open(f'files\{file}', encoding='utf8') as f:
-        content = f.readlines()
-        list_dict[file] = content
+def open_files(file_list):
+    for file in list_of_files:
+        with open(f'files\{file}', encoding='utf8') as f:
+            content = f.readlines()
+            list_dict[file] = content
+    return list_dict
 
 
-# new_file = str(sorted_dict)
-with open(file_name, 'w', encoding='utf8') as file:
-    for key, value in sorted(list_dict.items(), key = lambda item: len(item[1])):
-        file.write(key+ '\n')
-        file.write("Строк: "+ str(len(value))+ '\n')
-        for v in value:
-            file.write(v)
-        file.write('\n\n')
+
+def write_file(file_list):
+    file_dict = open_files(file_list)
+    with open(file_name, 'w', encoding='utf8') as file:
+        for key, value in sorted(list_dict.items(), key = lambda item: len(item[1])):
+            file.write(key+ '\n')
+            file.write("Строк: "+ str(len(value))+ '\n')
+            for v in value:
+                file.write(v)
+            file.write('\n\n')
+
+write_file(list_of_files)
+
 
 
 
